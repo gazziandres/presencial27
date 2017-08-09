902399a1e843e60c860fb51b69c7ecd6e0c4f67f
gazzi <gazzi.andres@gmail.com>
2017-08-08 20:40:47 -0400

    first commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,27 @@
+class ProductsController < ApplicationController
+before_action :set_product, only: [:show, :edit, :update, :destroy]
+
+
+  def create
+    @product = Product.new(product_params)
+
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.json { render :show, status: :created, location: @product }
+      else
+        format.html { render :new }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+        redirect_to category_path
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :price)
+  end
+end
