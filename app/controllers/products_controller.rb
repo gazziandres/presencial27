e46a5c27c6b98682cc4be306ed603fe1ef6668a9
gazzi <gazzi.andres@gmail.com>
2017-08-08 22:01:28 -0400

    third commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,27 +1,35 @@
-class ProductsController < ApplicationController
-before_action :set_product, only: [:show, :edit, :update, :destroy]
+  class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
 
 
-  def create
-    @product = Product.new(product_params)
-
-    respond_to do |format|
-      if @product.save
-        format.html { redirect_to @product.category, notice: 'Product was successfully created.' }
-        format.json { render :show, status: :created, location: @product }
-      else
-        format.html { render :new }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-        redirect_to category_path
+    def create
+      @product = Product.new(product_params)
+      respond_to do |format|
+        if @product.save
+          format.html { redirect_to @product.category, notice: 'Product was successfully created.' }
+          format.json { render :show, status: :created, location: @product }
+        else
+          format.html { render :new }
+          format.json { render json: @product.errors, status: :unprocessable_entity }
+        end
+    end
   end
 
-  private
-
-  def set_product
-    @product = Product.find(params[:id])
+    def destroy
+      @product.destroy
+        respond_to do |format|
+          format.html { redirect_to @product.category, notice: 'Product was successfully destroyed.' }
+          format.json { head :no_content }
+      end
+    end
   end
 
-  def product_params
-    params.require(:product).permit(:name, :price)
-  end
-end
+    private
+
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    def product_params
+      params.require(:product).permit(:name, :price, :category_id)
+    end
